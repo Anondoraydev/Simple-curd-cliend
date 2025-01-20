16dfb39a0f721429ea7649aa035eb09cde8670c7
AnondoRayDev <anondo554@gmail.com>
2025-01-20 22:54:32 +0600

    update done

--- src/Update.jsx
+++ src/Update.jsx
@@ -2,37 +2,40 @@ import { useLoaderData } from "react-router-dom";
 
 
 const Update = () => {
-    const lodeedUser = useLoaderData();
+    const lodedUser = useLoaderData();
     const heandealUpdate = event => {
         event.preventDefault();
         const form = event.target;
-        const name = form.name.value
-        const email = form.email.value
+        const name = form.name.value;
+        const email = form.email.value;
         console.log(email, name);
-        const updatedUser = { email, name }
+        const updatedUser = { email, name };
 
-        fetch(`http://localhost:5000/users/${lodeedUser._id}`, {
+        fetch(`http://localhost:5000/users/${lodedUser._id}`, {
             method: 'PUT',
             headers: {
-                "content-type": "application/json"
+                "content-type": "application/json",
             },
             body: JSON.stringify(updatedUser)
         })
             .then(res => res.json())
             .then(data => {
                 console.log(data);
+                if(data.matchedCount>0){
+                    alert("user Update successfully")
+                }
 
             })
     }
 
     return (
         <div>
-            <h2>Update Infometion of {lodeedUser.name}</h2>
+            <h2>Update Infometion of {lodedUser.name}</h2>
 
             <form onSubmit={heandealUpdate}>
-                <input type="text" name="name" defaultValue={lodeedUser?.name} id="" />
+                <input type="text" name="name" defaultValue={lodedUser?.name} id="" />
                 <br />
-                <input type="email" name="email" defaultValue={lodeedUser?.email} id="" />
+                <input type="email" name="email" defaultValue={lodedUser?.email} id="" />
                 <br />
                 <input type="submit" value="Update" />
             </form>
